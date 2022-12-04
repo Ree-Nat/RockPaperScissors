562936704f7f0288c68475bbca936dec2095b340
Fushi Yaki <53132424+Ree-Nat@users.noreply.github.com>
2022-12-03 17:41:42 -0800

    initialize lessons and files

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+
+Create a function getComputerChoice
+ -Will return "Rock Paper Scissors"
+
+Create a function that palys a single round of Rock Paper Scissors 
+-Take two parameters playerSelection and computerSelection
+-Compares them
+-Decleares the winner
+-Return a string "You lose! Paper Beats rock"
+-Make case insensitive
+
+Use prompt() to get user input
+*/
+
+
